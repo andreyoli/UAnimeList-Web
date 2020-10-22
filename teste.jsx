36a37209fe46e50fde91b636936941ec05749a26
Andrey <andrey.o.paula@gmail.com>
2020-10-22 15:27:39 -0300

    Removed test file

--- teste.jsx
+++
@@ -1,8 +0,0 @@
-export const cropString = (string, length, message) => {
-  if (string != null) {
-    const cropedString = `${string.substr(0, length)}...`
-    return cropedString
-  } else {
-    return message
-  }
-}
